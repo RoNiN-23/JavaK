1a39dc3a08b527887b365246824e3cff592999da
RoNiN-23 <70800758+RoNiN-23@users.noreply.github.com>
2021-05-04 20:04:45 +0300

    deleted all

--- carsharingCompany/src/main/java/ru/mirea/carsharing/repo/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.carsharing.repo;
-
-import org.springframework.data.repository.CrudRepository;
-import ru.mirea.carsharing.domain.User;
-
-public interface UserRepo extends CrudRepository <User, Long>{
-    User findByUsername(String name);
-}
