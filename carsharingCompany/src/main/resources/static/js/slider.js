fb83905492e81dc8f9a421cba8eb63d6fb0d326c
RoNiN-23 <70800758+RoNiN-23@users.noreply.github.com>
2021-05-25 14:16:27 +0300

    deleted the first version of the project
    I deleted it because I couldn't run tests because of a bug

--- carsharingCompany/src/main/resources/static/js/slider.js
+++
@@ -1,9 +0,0 @@
-var i=0;
-var image=document.getElementById("image");
-// Добавте свои картинки через запятую
-var imgs=new Array('@{/images/KiaBig.jpg}','@{/images/VolkBig.jpg}', '@{/images/MerceGLABig.jpg}'
-    , '@{/images/MerceCLABig.jpg}', '@{/images/RenaultBig.jpg}');
-function imgsrc() {
-    i++;
-    image.src=imgs[i];
-}
